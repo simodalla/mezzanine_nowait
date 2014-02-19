55e63b35357c6d17de13043d4e5a0800ecd5f21d
Simone Dalla <simodalla@gmail.com>
2014-02-19 19:08:49 +0100

    fixing

---
+++ mezzanine_nowait/test_views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+
+class DemoTest(TestCase):
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_2(self):
+        self.assertEqual(1, 1)
+
+    def test_3(self):
+        self.assertEqual(1, 1)
