e7fbb6d4f62fd4e521f916d714514bac5610cfb5
Simone Dalla <simodalla@gmail.com>
2014-02-26 21:07:47 +0100

    add root_app_page_get_or_create and relative tests

--- nowait/tests/test_utils.py
+++ nowait/tests/test_utils.py
@@ -1,20 +1,67 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
+from django.test.utils import override_settings
 
-from mezzanine.pages.models import RichTextPage
+from mezzanine.conf import settings
+from mezzanine.pages.models import RichTextPage, Link
 
 from ..utils import root_app_page_get_or_create
 
+NOWAIT_ROOT_SLUG = 'nowait-root-page'
 
+
+@override_settings(NOWAIT_ROOT_SLUG=NOWAIT_ROOT_SLUG)
 class RootAppPageCreationTest(TestCase):
 
     def setUp(self):
-        pass
+        self.defaults = {'title': 'Nowait Root Page',
+                         'content': '<p>Content</p>'}
+
+    def test_call_root_app_page_get_or_create_without_no_wait_slug_setting(
+            self):
+        with override_settings(NOWAIT_ROOT_SLUG=None):
+            self.assertRaises(
+                ImproperlyConfigured,
+                root_app_page_get_or_create,
+                RichTextPage, 'nowait', **self.defaults)
+
+    def test_strip_border_slashes(self):
+        with override_settings(NOWAIT_ROOT_SLUG='/%s/' % NOWAIT_ROOT_SLUG):
+            page, created = root_app_page_get_or_create(
+                RichTextPage, 'nowait', **self.defaults)
+            self.assertEqual(page.slug, NOWAIT_ROOT_SLUG)
+
+    def test_no_strip_border_slashes_if_slug_is_root_slash(self):
+        with override_settings(NOWAIT_ROOT_SLUG='/'):
+            page, created = root_app_page_get_or_create(
+                RichTextPage, 'nowait', **self.defaults)
+            self.assertEqual(page.slug, '/')
 
     def test_create_root_page_return_page(self):
-        title = 'Nowait Root Page'
-        slug = 'nowait-root-page'
-        page, create = root_app_page_get_or_create(RichTextPage, slug, title)
+        page, created = root_app_page_get_or_create(
+            RichTextPage, 'nowait', **self.defaults)
+        self.assertTrue(isinstance(page, RichTextPage))
+        self.assertTrue(created)
+        self.assertEqual(page.slug, settings.NOWAIT_ROOT_SLUG)
+        for field in self.defaults:
+            self.assertEqual(getattr(page, field), self.defaults[field])
+
+    def test_get_root_page_with_more_pages_with_same_slug(self):
+        already_root_page = RichTextPage.objects.create(
+            slug=NOWAIT_ROOT_SLUG, **self.defaults)
+        self.assertEqual(RichTextPage.objects.count(), 1)
+        page, created = root_app_page_get_or_create(
+            RichTextPage, 'nowait', **self.defaults)
+        self.assertEqual(RichTextPage.objects.count(), 1)
+        self.assertFalse(created)
+        self.assertEqual(already_root_page.pk, page.pk)
+        self.assertEqual(page.slug, settings.NOWAIT_ROOT_SLUG)
+        for field in self.defaults:
+            self.assertEqual(getattr(page, field), self.defaults[field])
+
+
+
 
--- nowait/utils.py
+++ nowait/utils.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ImproperlyConfigured
+
 from mezzanine.conf import settings
+from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
 
 
 class PageContextTitleMixin(object):
@@ -27,10 +30,16 @@ class PageContextTitleMixin(object):
         return context
 
 
-def root_app_page_get_or_create(model, slug, title, **defaults):
+def root_app_page_get_or_create(model_page, app_label, **defaults):
     settings.use_editable()
-    root_slug = settings.NOWAIT_ROOT_SLUG.lstrip('/').rstrip('/')
-    root_page, create = model.objects.get_or_create(slug=slug, title=title,
-                                                    defaults=defaults)
+    setting_root_slug_key = '{0}_ROOT_SLUG'.format(app_label.upper())
+    slug = getattr(settings, setting_root_slug_key, None)
+    if not slug:
+        raise ImproperlyConfigured("The {0} setting must not be empty.".format(
+            setting_root_slug_key))
+    if slug != '/':
+        slug = slug.lstrip('/').rstrip('/')
+    root_page, create = model_page.objects.get_or_create(
+        slug=slug, defaults=defaults)
     return root_page, create
 
