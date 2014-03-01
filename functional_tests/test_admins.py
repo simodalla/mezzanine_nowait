c38ad4f02d6f036f23ef6d3c1e033e9843fb068a
Simone Dalla <simodalla@gmail.com>
2014-03-01 11:22:58 +0100

    comment unused tests

--- functional_tests/test_admins.py
+++ functional_tests/test_admins.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
-
-from unittest import skip
-from django.contrib.admin.templatetags.admin_urls import admin_urlname
-
-from .base import FunctionalTest
-from nowait.tests.factories import AdminF
-from nowait.models import BookingType
-
-
-@skip
-class AdminTest(FunctionalTest):
-    def setUp(self):
-        super(AdminTest, self).setUp()
-        self.admin = AdminF()
-        self.create_pre_authenticated_session(self.admin)
-
-    def test_add_booking_type(self):
-        self.browser.get(
-            self.get_url(admin_urlname(BookingType._meta, 'changelist')))
+# from __future__ import unicode_literals, absolute_import
+#
+# from django.contrib.admin.templatetags.admin_urls import admin_urlname
+#
+# from .base import FunctionalTest
+# from nowait.tests.factories import AdminF
+# from nowait.models import BookingType
+#
+#
+# class AdminTest(FunctionalTest):
+#     def setUp(self):
+#         super(AdminTest, self).setUp()
+#         self.admin = AdminF()
+#         self.create_pre_authenticated_session(self.admin)
+#
+#     def test_add_booking_type(self):
+#         self.browser.get(
+#             self.get_url(admin_urlname(BookingType._meta, 'changelist')))
