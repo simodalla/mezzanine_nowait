6fda0c6191dc2a57b44daa60ec4c8247a02f7b97
Simone Dalla <simodalla@gmail.com>
2014-03-17 18:19:05 +0100

    renaming of requiements files

--- runtests.py
+++ runtests.py
@@ -24,7 +24,7 @@ try:
 
     from django_nose import NoseTestSuiteRunner
 except ImportError:
-    raise ImportError("To fix this error, run: pip install -r requirements-test.txt")
+    raise ImportError("To fix this error, run: pip install -r test.txt")
 
 
 def run_tests(*test_args):
